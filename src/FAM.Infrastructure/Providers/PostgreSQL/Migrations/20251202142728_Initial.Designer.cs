86bfa8b8b26c91b01b47b1f8d024369b5a3ec300
Vũ Văn Anh <vuvananh010203@gmail.com>
2025-12-02 21:30:52 +0700

    Refactor code structure for improved readability and maintainability

--- src/FAM.Infrastructure/Providers/PostgreSQL/Migrations/20251202142728_Initial.Designer.cs
+++ src/FAM.Infrastructure/Providers/PostgreSQL/Migrations/20251202142728_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FAM.Infrastructure.Providers.PostgreSQL.Migrations
 {
     [DbContext(typeof(PostgreSqlDbContext))]
-    [Migration("20251202110813_Initial")]
+    [Migration("20251202142728_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
